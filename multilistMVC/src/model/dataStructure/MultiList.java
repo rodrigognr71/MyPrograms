2ed1280ce2a24eb0c08805d29a6d8cfe1c3b49da
Rodrigo Espinoza <rodrigognr71@gmail.com>
2019-07-15 20:35:28 -0400

    Update MultiList.java

--- multilistMVC/src/model/dataStructure/MultiList.java
+++ multilistMVC/src/model/dataStructure/MultiList.java
@@ -3,13 +3,13 @@ package model.dataStructure;
 import model.Student;
 import model.Subject;
 
-import java.util.NoSuchElementException;
 
 public class MultiList {
     private NodeStudent firstStudent;
     private NodeStudent lastStudent;
     private NodeSubject firstSubject;
     private NodeSubject lastSubject;
+    private NodeStudent current;
 
     public void addStudent(Student student) {
         final NodeStudent currentLast = lastStudent;
@@ -21,6 +21,7 @@ public class MultiList {
         else {
             currentLast.nextNodeStudent = newNode;
         }
+        current = lastStudent;
     }
 
     public void addSubject(Subject subject){
@@ -35,10 +36,30 @@ public class MultiList {
         }
     }
 
-    public void nextStudent(Student student){
-       
+    public Student nextStudent(){
+        if (current != null && current.nextNodeStudent != null){
+            current = current.nextNodeStudent;
+        }
+        return current.student;
     }
 
+    public Student searchStudent(Student student){
+        current = searchNodeStudent(firstStudent);
+        return current.student;
+    }
+
+    private NodeStudent searchNodeStudent(NodeStudent nodeStudent){
+        NodeStudent current = nodeStudent;
+        while(current != null && current.nextNodeStudent != null){
+            if(current.student.equals(nodeStudent.student)) {
+                break;
+            }
+            else {
+                current = current.nextNodeStudent;
+            }
+        }
+        return current;
+    }
 
 
 }
